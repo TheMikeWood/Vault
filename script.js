9230b98b4752a59a105eb659ff94b75301fb5a2d
Michael Woodard <mr.m.woodard@gmail.com>
2024-12-11 16:20:58 -0500

    Upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Create message string
+const string1 =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: ";
+// Declare first variable
+const num1 = 5 * 2;
+// Declare second variable
+const num2 = 42 - (27 % 5);
+// Declare third variable
+const num3 = 117 / 3;
+// concatonate consts inside of an alert popup
+alert(string1 + num1 + " - " + num2 + " - " + num3);
+console.log("DOM fully loaded and parsed");
